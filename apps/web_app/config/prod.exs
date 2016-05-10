0c118db65703c0027ab2aad0a9d76c79cac6f7f0
Stan Krowinski <przemyslaw.krowinski@profinda.com>
2016-05-11 00:54:05 +0200

    Add meningful domain name

--- apps/web_app/config/prod.exs
+++ apps/web_app/config/prod.exs
@@ -13,8 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :web_app, WebApp.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "http", host: "real-news.herokuapp.com", port: 80],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "www.greetingsfellowhumans.com", port: 80],
+  #force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
